b6d664a6055879aeec540d933b746ffa0eb74c01
Norma Lopez <norma_l304@yahoo.com>
2020-05-27 10:28:58 -0600

    Add method for ticket_lottery_contestants

--- lib/museum.rb
+++ lib/museum.rb
@@ -33,6 +33,12 @@ class Museum
         end
       end
     end
-    sorted 
+    sorted
+  end
+
+  def ticket_lottery_contestants(exhibit)
+    @patrons.find_all do |patron|
+      patron.spending_money < exhibit.cost 
+    end
   end
 end
